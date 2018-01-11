1d977bedccde2c5bfc3cad9055c30ec59888f0a0
choi jae hyuk <kazaana2009@naver.com>
2018-01-11 15:58:23 +0900

    api 완성

--- Food_Truck/src/main/java/com/foodtruck/controller/FoodTruckController.java
+++ Food_Truck/src/main/java/com/foodtruck/controller/FoodTruckController.java
@@ -119,92 +119,109 @@ public class FoodTruckController {
 	@RequestMapping("/api")
 	public String inputAddr(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// logger.info("PublicData2");
-
-		request.setCharacterEncoding("utf-8");
-		response.setContentType("text/html; charset=utf-8");
-
-		String addr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival?ServiceKey=";
-		String serviceKey = "H%2F5lCacGTb8Gu0hK0t%2FZ%2BR04rZ7FtCgprC9i3gXHYFbGvnPOF1UikRkxLw07AXydV%2BN7SBJCExRUanyQ2DRgVQ%3D%3D";
-
-		String parameter = "";
-		// serviceKey = URLEncoder.encode(serviceKey,"utf-8");
-
-		PrintWriter out = response.getWriter();
-		Date date = new Date();
-		SimpleDateFormat sdformat = new SimpleDateFormat("YYYYMMdd");
-		String today = sdformat.format(date);// �쁽�옱�궇吏�
-		// PrintWriter out = new PrintWriter(new OutputStream
-		// Writer(response.getOutputStream(),"KSC5601"));
-		// ServletOutputStream out = response.getOutputStream();
-		parameter = parameter + "&" + "areaCode=1";
-		parameter = parameter + "&" + "cat1=A02";
-		parameter = parameter + "&" + "cat2=A0207";
-		parameter = parameter + "&" + "eventStartDate=" + today;
-		parameter = parameter + "&" + "eventEndDate=20191231";
-		parameter = parameter + "&" + "pageNo=1&numOfRows=100";
-		parameter = parameter + "&" + "arrange=B";
-		parameter = parameter + "&" + "listYN=Y";
-		parameter = parameter + "&" + "MobileOS=ETC";
-		parameter = parameter + "&" + "MobileApp=aa";
-		parameter = parameter + "&" + "_type=json";
-
-		addr = addr + serviceKey + parameter;
-		URL url = new URL(addr);
-
-		System.out.println(addr);
-
-		// BufferedReader in = new BufferedReader(new
-		// InputStreamReader(url.openStream(), "UTF-8"));
-
-		InputStream in = url.openStream();
-		// CachedOutputStream bos = new CachedOutputStream();
-		ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
-		IOUtils.copy(in, bos1);
-		in.close();
-		bos1.close();
-
-		String mbos = bos1.toString("UTF-8");
-
-		byte[] b = mbos.getBytes("UTF-8");
-		String s = new String(b, "UTF-8");
-		out.println(s);
-
-		/* json parsing */
-
-		JSONParser parser = new JSONParser();
-		Object obj = parser.parse(s);
-		JSONObject json = (JSONObject) obj;
-
-		// json.put("data", data);
-
-		JSONObject js = (JSONObject) json.get("response");
-		JSONObject jj = (JSONObject) js.get("body");
-		JSONObject items = (JSONObject) jj.get("items");
-		JSONArray jArray = (JSONArray) items.get("item");
-
 		List<ApiVO> list = new ArrayList<ApiVO>();
-		for (int j = 0; j < jArray.size(); j++) {
-			JSONObject a = (JSONObject) jArray.get(j);
-			ApiVO vo = new ApiVO();
+		int num[] = { 1, 2, 3, 4, 5, 6, 7, 8, 31, 32 };
 
-			vo.setAddr1(a.get("addr1").toString());
-			vo.setTitle(a.get("title").toString());
-			vo.setMapx(a.get("mapx").toString());
-			vo.setMapy(a.get("mapy").toString());
-			vo.setEventstartdate(a.get("eventstartdate").toString());
-			vo.setEventenddate(a.get("eventenddate").toString());
-			vo.setTel(a.get("tel").toString());
-			vo.setFirstimage(a.get("firstimage").toString());
-			vo.setFirstimage2(a.get("firstimage2").toString());
-			list.add(vo);
+		for (int q = 0; q < num.length; q++) {
+			try {
+				int code = num[q];
+				System.out.println(code);
+				request.setCharacterEncoding("utf-8");
+				response.setContentType("text/html; charset=utf-8");
 
+				String addr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival?ServiceKey=";
+				String serviceKey = "H%2F5lCacGTb8Gu0hK0t%2FZ%2BR04rZ7FtCgprC9i3gXHYFbGvnPOF1UikRkxLw07AXydV%2BN7SBJCExRUanyQ2DRgVQ%3D%3D";
+
+				String parameter = "";
+				// serviceKey = URLEncoder.encode(serviceKey,"utf-8");
+
+				PrintWriter out = response.getWriter();
+				Date date = new Date();
+				SimpleDateFormat sdformat = new SimpleDateFormat("YYYYMMdd");
+				String today = sdformat.format(date);// �쁽�옱�궇吏�
+				// PrintWriter out = new PrintWriter(new OutputStream
+				// Writer(response.getOutputStream(),"KSC5601"));
+				// ServletOutputStream out = response.getOutputStream();
+				parameter = parameter + "&" + "areaCode=" + code;
+				parameter = parameter + "&" + "cat1=A02";
+				parameter = parameter + "&" + "cat2=A0207";
+				parameter = parameter + "&" + "eventStartDate=" + today;
+				parameter = parameter + "&" + "eventEndDate=20191231";
+				parameter = parameter + "&" + "pageNo=1&numOfRows=100";
+				parameter = parameter + "&" + "arrange=B";
+				parameter = parameter + "&" + "listYN=Y";
+				parameter = parameter + "&" + "MobileOS=ETC";
+				parameter = parameter + "&" + "MobileApp=aa";
+				parameter = parameter + "&" + "_type=json";
+
+				addr = addr + serviceKey + parameter;
+				URL url = new URL(addr);
+
+				System.out.println(addr);
+
+				// BufferedReader in = new BufferedReader(new
+				// InputStreamReader(url.openStream(), "UTF-8"));
+
+				InputStream in = url.openStream();
+				// CachedOutputStream bos = new CachedOutputStream();
+				ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
+				IOUtils.copy(in, bos1);
+				in.close();
+				bos1.close();
+
+				String mbos = bos1.toString("UTF-8");
+
+				byte[] b = mbos.getBytes("UTF-8");
+				String s = new String(b, "UTF-8");
+				out.println(s);
+
+				/* json parsing */
+				System.out.println(s);
+				JSONParser parser = new JSONParser();
+				Object obj = parser.parse(s);
+				JSONObject json = (JSONObject) obj;
+
+				// json.put("data", data);
+
+				JSONObject js = (JSONObject) json.get("response");
+				JSONObject jj = (JSONObject) js.get("body");
+				JSONObject items = (JSONObject) jj.get("items");
+				JSONArray jArray = (JSONArray) items.get("item");
+
+				for (int j = 0; j < jArray.size(); j++) {
+					JSONObject a = (JSONObject) jArray.get(j);
+					ApiVO vo = new ApiVO();
+					vo.setAddr1(a.get("addr1").toString());
+					vo.setTitle(a.get("title").toString());
+					vo.setMapx(a.get("mapx").toString());
+					vo.setMapy(a.get("mapy").toString());
+					vo.setEventstartdate(a.get("eventstartdate").toString());
+					vo.setEventenddate(a.get("eventenddate").toString());
+					vo.setTel(a.get("tel").toString());
+					vo.setFirstimage((String) a.get("firstimage"));
+					vo.setFirstimage2((String) a.get("firstimage2"));
+					list.add(vo);
+
+				}
+			} catch (Exception e) {
+				System.out.println("축제정보없음");
+			}
 		}
 		for (int j = 0; j < list.size(); j++) {
 
-			System.out.println("list:" + list.get(j).getFirstimage());
+			System.out.println(list.get(j).getAddr1());
+			System.out.println(list.get(j).getTitle());
+			System.out.println(list.get(j).getEventenddate());
+			System.out.println(list.get(j).getEventstartdate());
+			System.out.println(list.get(j).getMapx());
+			System.out.println(list.get(j).getMapy());
+			System.out.println(list.get(j).getTel());
+			System.out.println(list.get(j).getFirstimage());
+			System.out.println(list.get(j).getFirstimage2());
 
 		}
-		
+
 		return "nav/recommend";
+
 	}
 }
