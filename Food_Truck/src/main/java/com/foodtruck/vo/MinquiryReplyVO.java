8fc181516c61838cf32dbfc71e81959a6bf4abd0
choi jae hyuk <kazaana2009@naver.com>
2018-01-24 13:06:49 +0900

    일반회원-관리자 Q&A 완료

---
+++ Food_Truck/src/main/java/com/foodtruck/vo/MinquiryReplyVO.java
@@ -0,0 +1,44 @@
+package com.foodtruck.vo;
+
+public class MinquiryReplyVO {
+	
+	private String replyScNo;
+	private String memId;
+	private int qaScNo;
+	private String replyScContent;
+	private String replyScReg;
+	
+	
+	public String getReplyScNo() {
+		return replyScNo;
+	}
+	public void setReplyScNo(String replyScNo) {
+		this.replyScNo = replyScNo;
+	}
+	public String getMemId() {
+		return memId;
+	}
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+	public int getQaScNo() {
+		return qaScNo;
+	}
+	public void setQaScNo(int qaScNo) {
+		this.qaScNo = qaScNo;
+	}
+	public String getReplyScContent() {
+		return replyScContent;
+	}
+	public void setReplyScContent(String replyScContent) {
+		this.replyScContent = replyScContent;
+	}
+	public String getReplyScReg() {
+		return replyScReg;
+	}
+	public void setReplyScReg(String replyScReg) {
+		this.replyScReg = replyScReg;
+	}
+	
+	
+}
