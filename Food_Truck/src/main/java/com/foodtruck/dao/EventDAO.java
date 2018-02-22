4881b8dc3a538f006d6ba3ceb3439dba8db557ea
LAPTOP-6QKFSP71\손혜여니 <ououou25>
2018-02-22 20:54:32 +0900

    eventDAO / eventService 정리

--- Food_Truck/src/main/java/com/foodtruck/dao/EventDAO.java
+++ Food_Truck/src/main/java/com/foodtruck/dao/EventDAO.java
@@ -3,24 +3,31 @@ package com.foodtruck.dao;
 import java.util.List;
 
 import com.foodtruck.vo.EventVO;
-import com.foodtruck.vo.NoticeVO;
 
 public interface EventDAO {
 	
+	// 등록된 이벤트 리스트
 	public List<EventVO> getEventBoardList(int NpageNo);
 	
-	public EventVO getEvent(int noticeNo);
+	// 이벤트 상세보기
+	public EventVO getEvent(int eventNo);
 	
+	// 이벤트 등록
 	public int insertEvent(EventVO vo);
 	
+	// 이벤트 수정
 	public int updateEvent(EventVO vo);
 	
-	public int countEvent(int noticeNo);
+	// 조회수 
+	public int countEvent(int eventNo);
 	
+	// 이벤트 삭제
 	public int deleteEvent(EventVO vo);
 	
+	// 이벤트 총갯수
 	public int getCountEvent();
 	
 	// menuBoard에 이벤트 진행중이면, 푸드트럭 이름 옆에 이벤트 띄워 주려고
 	public List<EventVO> progressEvent();
+	
 }
--- Food_Truck/src/main/java/com/foodtruck/service/EventService.java
+++ Food_Truck/src/main/java/com/foodtruck/service/EventService.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.foodtruck.dao.EventDAO;
-import com.foodtruck.dao.MemberDAO;
-import com.foodtruck.dao.NoticeDAO;
 import com.foodtruck.vo.EventVO;
-import com.foodtruck.vo.NoticeVO;
 
 @Service
 public class EventService {
@@ -18,36 +15,44 @@ public class EventService {
 	@Autowired
 	private SqlSessionTemplate sessionTemplate;
 	
+	// 등록된 이벤트 리스트
 	public List<EventVO> getEventBoardList(int NpageNo) {
 		EventDAO dao = sessionTemplate.getMapper(EventDAO.class);
 		return dao.getEventBoardList(NpageNo);
 	}
 	
-	public EventVO getEvent(int noticeNo) {
+	// 이벤트 상세보기
+	public EventVO getEvent(int eventNo) {
 		EventDAO dao = sessionTemplate.getMapper(EventDAO.class);
-		return dao.getEvent(noticeNo);
+		return dao.getEvent(eventNo);
 	}
 	
+	// 이벤트 등록
 	public int insertEvent(EventVO vo) {
 		EventDAO dao = sessionTemplate.getMapper(EventDAO.class);
 		
 		return dao.insertEvent(vo);
 	}
 	
+	// 이벤트 수정
 	public int updateEvent(EventVO vo) {
 		EventDAO dao = sessionTemplate.getMapper(EventDAO.class);
 		return dao.updateEvent(vo);
 	}
 	
-	public int countEvent(int noticeNo) {
+	// 조회수 	
+	public int countEvent(int eventNo) {
 		EventDAO dao = sessionTemplate.getMapper(EventDAO.class);
-		return dao.countEvent(noticeNo);
+		return dao.countEvent(eventNo);
 	}
 	
+	// 이벤트 삭제
 	public int deleteEvent(EventVO vo) {
 		EventDAO dao = sessionTemplate.getMapper(EventDAO.class);
 		return dao.deleteEvent(vo);
 	}
+	
+	// 이벤트 총갯수
 	public int getCountEvent() {
 		EventDAO dao = sessionTemplate.getMapper(EventDAO.class);
 		return dao.getCountEvent();
@@ -58,6 +63,5 @@ public class EventService {
 		EventDAO dao = sessionTemplate.getMapper(EventDAO.class);
 		return dao.progressEvent();
 	}
-
 
 }
