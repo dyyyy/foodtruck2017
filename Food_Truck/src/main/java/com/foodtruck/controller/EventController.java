8d94d85f5b9584c9b9881cfad58634002f1e5574
LAPTOP-6QKFSP71\손혜여니 <ououou25>
2018-02-22 15:00:53 +0900

    eventController 정리

--- Food_Truck/src/main/java/com/foodtruck/controller/EventController.java
+++ Food_Truck/src/main/java/com/foodtruck/controller/EventController.java
@@ -3,7 +3,6 @@ package com.foodtruck.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,49 +23,47 @@ public class EventController {
 
 	@Autowired
 	EventService eventService;
-
 	@Autowired
 	MemberService memberService;
-	
 	@Autowired
 	FoodTruckService foodtruckService;
-	
 	@Autowired
 	SellerService sellerService;
 
-	// ��������
+	// 이벤트 페이지
 	@RequestMapping("/eventBoard")
-	public String getEventBoardList(Model model, HttpServletResponse response, HttpSession session, MemberVO mvo,
-			HttpServletRequest request, @RequestParam("pageNo") int pageNo) {
+	public String getEventBoardList(Model model, HttpSession session, HttpServletRequest request, 
+									@RequestParam("pageNo") int pageNo) {
 		// 페이징처리
 		int NpageNo = 1;
 		if (pageNo != 1) {
 			NpageNo = (pageNo - 1) * 10 + 1;
 		}
-		int count =eventService.getCountEvent();
 		
+		int count = eventService.getCountEvent();
 		List<EventVO> list = eventService.getEventBoardList(NpageNo);
+
 		if (list != null) {
 			model.addAttribute("rank", list);
 		}
+		
 		request.setAttribute("pageNo", pageNo);
 		request.setAttribute("pagecount", count);
 		return "nav/eventBoard";
 	}
 
-	// �������� ��
+	// 이벤트 상세보기
 	@RequestMapping("/detailEventForm")
 	public String detailEventForm(@RequestParam("eventNo") int eventNo, HttpServletRequest request) {
+	
 		EventVO vo = eventService.getEvent(eventNo);
-
 		request.setAttribute("vo", vo);
-
-		eventService.countEvent(vo.getEventNo()); // ��ȸ��
-
+		eventService.countEvent(vo.getEventNo());
 		return "nav/detailEventForm";
+		
 	}
 
-	// �������� �۾���
+	// 이벤트 등록하기
 	@RequestMapping("/writeEventForm")
 	public String writeEvent(HttpServletRequest request, HttpSession session) {
 		
@@ -74,44 +71,48 @@ public class EventController {
 		request.setAttribute("licenseList", sellerService.getLicense(memId));
 		
 		return "nav/writeEventForm";
+		
 	}
 
-	// �������� ���
+	// 이벤트 등록폼에서 넘어온 값들 디비에 저장
 	@RequestMapping("/insertEvent")
-	public String insertEvent(HttpSession session, EventVO vo, MemberVO mvo) {
+	public String insertEvent(HttpSession session, EventVO vo) {
 
-		MemberVO m = (MemberVO) session.getAttribute("member");
-		vo.setMemId(m.getMemberId()); 
+		MemberVO mvo = (MemberVO) session.getAttribute("member");
+		vo.setMemId(mvo.getMemberId()); 
 		System.out.println(vo.getEventTitle());
-		// vo.setMemberId(mvo.getMemberId());
 
 		eventService.insertEvent(vo);
 		return "redirect:/eventBoard?pageNo=1";
+		
 	}
 
-	// �������� ���� ��
+	// 수정하기 눌렀을 떄, 수정폼
 	@RequestMapping("/updateEventForm")
-	public String updateEventForm(HttpServletRequest request, @RequestParam("eventNo") int eventNo) {
+	public String updateEventForm(HttpServletRequest request, @RequestParam("eventNo") int eventNo, HttpSession session) {
+
 		EventVO vo = eventService.getEvent(eventNo);
 		request.setAttribute("event", vo);
 		return "nav/updateEventForm";
+		
 	}
 
-	// �������� ����
+	// 수정폼에서 넘어온 값들 디비에 저장
 	@RequestMapping("/updateEvent")
 	public String updateEvent(EventVO vo) {
+		
 		eventService.updateEvent(vo);
-
 		System.out.println("update Controller");
 		return "redirect:/eventBoard?pageNo=1";
+		
 	}
 
-	// �������� ����
+	// 이벤트 삭제
 	@RequestMapping("/deleteEvent")
 	public String deleteEvent(EventVO vo) {
+		
 		eventService.deleteEvent(vo);
-
 		return "redirect:/eventBoard?pageNo=1";
+		
 	}
-
 }
